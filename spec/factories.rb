17486ae8c6e56a8e4270ac1f669bab76bd84c8f8
Shaun Martin <polycube@live.com>
2013-05-14 22:34:35 -0500

    Lesson 7 part 1 finished

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name "Shaun Martin"
+		email "shaunmartin@uwalumni.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,13 @@ describe "User pages" do
 		it { should have_selector('h1', text: 'Sign Up')}
 		it { should have_selector('title', text: full_title('Sign Up'))}
 	end
+
+	describe "profile page" do
+		let(:foobar) { FactoryGirl.create(:user) }
+		before { visit user_path(foobar) }
+
+		it { should have_selector('h1', text: foobar.name) }
+		it { should have_selector('title', text: foobar.name) }
+	end
+
 end

417ebf79b8aa174863c0aa105674cb3b56db5d91
Shaun Martin <polycube@live.com>
2013-05-14 21:01:14 -0500

    Lesson 7 Start

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+
+  has_secure_password
+
+  # this is advanced, but necessary for unique emails
+  # because SQL is case insenstive
+  before_save { |user| user.email = user.email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]{2,}\z/i
+
+  # this will not work against a scenario where the same user clicks
+  # a submit button twice in rapid succession.
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  					uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
